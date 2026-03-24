4c9d980250f80b846385879e42d21a1298c4da5f
Al-Muhlisin <muhlisinaal@gmail.com>
2026-03-24 12:45:26 +0700

    Update RekeningValas.java

--- src/banking/RekeningValas.java
+++ src/banking/RekeningValas.java
@@ -3,5 +3,10 @@ package banking;
 public class RekeningValas extends Rekening implements TransferGlobal{
   private String jenisMataUang;
   private final double kursRate;
-  
+
+  public RekeningValas(String nomorRekening, double saldo, String namaPemilik, String jenisMataUang, double kursRate) {
+        super(nomorRekening, saldo, namaPemilik);
+        this.jenisMataUang = jenisMataUang;
+        this.kursRate = kursRate;
+  }
   
