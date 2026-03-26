498534a59d97bc68cf258d93fbe3256fcb759ce6
Aryan Zaky Prayogo <prayogoaryan63@gmail.com>
2026-03-26 23:23:02 +0700

    Remove comments from TransferGlobal interface

--- src/banking/TransferGlobal.java
+++ src/banking/TransferGlobal.java
@@ -1,19 +1,8 @@
 package banking;
 
-/**
- * Multiple inheritance interface.
- */
 public interface TransferGlobal extends TransaksiDigital, LayananInternasional {
 
-    /**
-     * Menjalankan transfer global.
-     * @return true bila berhasil
-     */
     boolean transferGlobal();
 
-    /**
-     * Verifikasi tambahan untuk transfer internasional.
-     * @return true bila lolos verifikasi
-     */
     boolean verifikasiInternasional();
 }
