a6a4a3276ae67aee0961ad2c2c6b4828ccf29b1b
Aryan Zaky Prayogo <prayogoaryan63@gmail.com>
2026-03-26 23:22:42 +0700

    Clean up TransaksiDigital interface documentation
    Removed comments and documentation from TransaksiDigital interface.

--- src/banking/TransaksiDigital.java
+++ src/banking/TransaksiDigital.java
@@ -1,18 +1,8 @@
 package banking;
 
-/**
- * Kontrak transaksi berbasis digital/online.
- */
 public interface TransaksiDigital extends Transaksi {
 
-    /**
-     * Menjalankan proses online.
-     */
     void prosesOnline();
 
-    /**
-     * Mengambil token digital aktif.
-     * @return token
-     */
     String getTokenDigital();
 }
