8a122c91ef38a2320c6c00c0e6a145ddd62c3591
Aryan Zaky Prayogo <prayogoaryan63@gmail.com>
2026-03-26 23:20:28 +0700

    Clean up comments in LayananInternasional interface
    Removed comments and documentation from LayananInternasional interface.

--- src/banking/LayananInternasional.java
+++ src/banking/LayananInternasional.java
@@ -1,18 +1,8 @@
 package banking;
 
-/**
- * Kontrak layanan lintas negara.
- */
 public interface LayananInternasional extends Transaksi {
 
-    /**
-     * Menjalankan alur internasional.
-     */
     void prosesInternasional();
 
-    /**
-     * Negara tujuan transaksi.
-     * @return negara tujuan
-     */
     String getNegaraTujuan();
 }
